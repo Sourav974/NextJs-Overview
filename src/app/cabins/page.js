c15f668b95762f3f47774a306970182a61e40446
Sourav974 <souravyadav97@gmail.com>
2025-06-25 15:10:48 +0530

    navigation  between pages

---
+++ src/app/cabins/page.js
@@ -0,0 +1,13 @@
+import Navigation from "../compoents/Navigation";
+
+const page = () => {
+  return (
+    <div>
+      <Navigation />
+
+      <h1>Cabins Page</h1>
+    </div>
+  );
+};
+
+export default page;
